36dcf248fcfe7aeaa07c8707b03a1d2c9c37cee2
ChristianBegines <christianbeginescarrasco@gmail.com>
2016-11-05 21:46:55 +0100

    feat:ya registra los logeados y  mantiene conversacion

---
+++ public/js/socket.js
@@ -0,0 +1,43 @@
+$(function(){
+  var socket=io.connect();
+  var $messageForm=$('#messageForm');
+  var $message= $('#message');
+  var $chat=$('#chat');
+  var $userForm=$('#userForm');
+  var $messageArea=$('#messageArea');
+  var $userFormArea=$('#userFormArea');
+  var $users=$('#users');
+  var $username=$('#username');
+
+
+  $messageForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('send message',$message.val());
+    $message.val('');
+  });
+
+  socket.on('new message',function(data){
+    if(data.msg!==''){
+        $chat.append( '<div class="well">'+data.msg+'</div>');
+    }
+  });
+
+  $userForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('new user',$username.val(),function(data){
+      if(data){
+        $userFormArea.hide();
+        $messageArea.show();
+      }
+    });
+    $username.val('');
+  });
+
+  socket.on('get users',function(data){
+    var html='';
+    for(var i=0;i<data.length;i++){
+      html+='<li class="list-group-item">'+data[i]+'</li>';
+    }
+    $users.html(html);
+  });
+});
